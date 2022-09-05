18a05a8f66be1ec659001695dfd6ea59004bb4fa
Zaxx <98286699+zaxxdev@users.noreply.github.com>
2022-09-05 08:33:20 +0200

    Updated APi key

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-/*SEARCH BY USING A CITY NAME (e.g. athens) OR A COMMA-SEPARATED CITY NAME ALONG WITH THE COUNTRY CODE (e.g. athens,gr)*/
 const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
 const msg = document.querySelector(".top-banner .msg");
 const list = document.querySelector(".ajax-section .cities");
 /*SUBSCRIBE HERE FOR API KEY: https://home.openweathermap.org/users/sign_up*/
-const apiKey = "4d8fb5b93d4af21d66a2948710284366";
+const apiKey = "2e2539e9ff7932cf70e25b68669982ac";
 
 form.addEventListener("submit", e => {
   e.preventDefault();
